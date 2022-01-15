59dddfaa202fb06b6d2e59d55ca293b5bf710d40
vukilis <vuklekic22@gmail.com>
2022-01-15 21:05:30 +0100

    first commit

---
+++ arch-setup.sh
@@ -0,0 +1,28 @@
+echo "--------------------------------------"
+echo "-----       ZSH INSTALATION      -----"
+echo "--------------------------------------"
+
+yay -S zsh zsh-syntax-highlighting autojump zsh-autosuggestions --noconfirm
+touch "$HOME/.cache/zshhistory"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+chsh $USER -s /bin/zsh
+#echo $SHELL
+
+echo "--------------------------------------"
+echo "-----         ZSH SETUP          -----"
+echo "--------------------------------------"
+
+sudo cp -r DroidSansMono /usr/share/fonts/DroidSansMono
+fc-cache -f -v
+cp .p10k.zsh $HOME/.p10k.zsh
+cp .zshrc $HOME/.zshrc
+cp .aliasrc $HOME/.aliasrc
+source ~/.zshrc
+source ~/.p10k.zsh
+
+echo "--------------------------------------"
+echo "-----            END             -----"
+echo "--------------------------------------"
+echo "REBOOT PC"
+
---
+++ debian-setup.sh
@@ -0,0 +1,27 @@
+echo "--------------------------------------"
+echo "-----       ZSH INSTALATION      -----"
+echo "--------------------------------------"
+
+apt install -y zsh zsh-syntax-highlighting autojump zsh-autosuggestions
+touch "$HOME/.cache/zshhistory"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+chsh $USER -s /bin/zsh
+#echo $SHELL
+
+echo "--------------------------------------"
+echo "-----         ZSH SETUP          -----"
+echo "--------------------------------------"
+
+sudo cp -r DroidSansMono /usr/share/fonts/DroidSansMono
+fc-cache -f -v
+cp .p10k.zsh $HOME/.p10k.zsh
+cp .zshrc $HOME/.zshrc
+cp .aliasrc $HOME/.aliasrc
+source ~/.zshrc
+source ~/.p10k.zsh
+
+echo "--------------------------------------"
+echo "-----            END             -----"
+echo "--------------------------------------"
+echo "REBOOT PC"
